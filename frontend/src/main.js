8d76059e0f06ea925575823aa9b7709ee5d48b34
Tom Klinovsky <tom@kiwinet.co.il>
2018-06-21 09:39:35 +0300

    apollo setup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
 import App from './App'
 
 Vue.config.productionTip = false
@@ -15,9 +19,26 @@ const router = new VueRouter({
   ]
 })
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
 new Vue({
   el: '#app',
   router,
+  provide: apolloProvider.provide(),
   components: { App },
   template: '<App/>'
 })
