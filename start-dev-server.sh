b65e5695cbde8d4fcdceab0935690b4312e07987
Tom Klinovsky <tom@kiwinet.co.il>
2018-06-26 14:12:36 +0300

    rebuilding mysql schema when starting dev server

--- start-dev-server.sh
+++ start-dev-server.sh
@@ -10,7 +10,8 @@ export FUNDME_HTTP_PORT=3000
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$(exec $DIR/dev-server-cleanup.sh)
+$(echo $DIR/mysql/rebuild-docker.sh);
+$(echo $DIR/dev-server-cleanup.sh);
 
 docker run --name $MYSQL_DOCKER_NAME -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD -d $MYSQL_DOCKER_IMAGE 2>&1 >/dev/null
 
