492f17ef891e68bf38d96de859190460d75dc448
Tom Klinovsky <tom@kiwinet.co.il>
2018-10-01 20:21:31 +0300

    fix an issue where foreign key constraints were case-insensitive

--- mysql/fundme.sql
+++ mysql/fundme.sql
@@ -1,28 +1,28 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE IF NOT EXISTS `Users` (
-  `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `github_id` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `github_login` varchar(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `github_access_token` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `github_id` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `github_login` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `github_access_token` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   UNIQUE KEY `user_id` (`user_id`(64)),
   UNIQUE (`github_id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `Projects` (
-  `project_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `project_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `name` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `online_repo` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `online_repo` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
+  `user_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `description` varchar(2048) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   UNIQUE KEY `project_id` (`project_id`(64)),
   FOREIGN KEY (`user_id`) REFERENCES Users(`user_id`) ON DELETE CASCADE
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `Challenges` (
-  `challenge_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `challenge_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `challenge_name` varchar(512) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `project_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `project_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `challenge_type` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `challenge_description` varchar(4096) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `amout_pledged` decimal(13,2) DEFAULT 0.00,
